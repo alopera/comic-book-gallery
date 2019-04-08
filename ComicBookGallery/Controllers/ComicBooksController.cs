8becf7f8f5e7a1f41fe5371ac8b4123c0e5f52f5
Alejandro lopera sanchez <alelopera@hotmail.com>
2019-04-08 13:51:08 -0500

    Added comic book repository

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,29 +10,13 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        ComicBookRepository comicBookRepository = new ComicBookRepository();
 
         public ActionResult Detail()
         {
-
-            var comicBook = new ComicBook() {
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[] {
-                    new Artist(){Name = "Dan Slott", Role = "Script"},
-                    new Artist(){Name = "Humberto Ramos", Role = "Pencils"},
-                    new Artist(){Name = "Victor Olazaba", Role = "Inks"},
-                    new Artist(){Name = "Edgar Delgado", Role = "Colors"},
-                    new Artist(){Name = "Chris Eliopoulos", Role = "Letters"},
-
-                },
-                IssueNumber = 700,
-                SeriesTitle = "The Amazing Spider-Man",
-                Favorite = true
-                
-            };
-
+            var comicBook = comicBookRepository.GetComicBook();
             return View(comicBook);
         }
-
 
     }
 }
