34920dfb4c1117c4c216b5e5656bcce6df90f780
Alejandro lopera sanchez <alelopera@hotmail.com>
2019-04-08 11:12:23 -0500

    Add View for Action Detail

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,15 +11,9 @@ namespace ComicBookGallery.Controllers
 
         public ActionResult Detail()
         {
-
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-
-            }
-
-            return Content("Hello from the comic books controller");
+            return View();
         }
+
 
     }
 }
